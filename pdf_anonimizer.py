e7945cb306adb4826858eb0a3060e130476943d6
sitrus <tomaszlaszczuk@gmail.com>
2015-04-08 19:37:42 +0200

    ManyFileUpload seems to be working

--- pdf_anonimizer.py
+++ pdf_anonimizer.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import zipfile
 from flask import Flask, request, redirect, render_template, flash, url_for, \
     send_from_directory, make_response
 from werkzeug import secure_filename
@@ -23,6 +24,17 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'Secret'
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+
+def validate_files(file_list):
+    flag = True
+    for uploaded_file in file_list:
+        if not allowed_file(uploaded_file.filename):
+            flash("Plik %s jest niepoprawny (czy to plik pdf?)"
+                  % uploaded_file.filename,
+                  "danger")
+            flag = False
+    return flag
 
 
 def clean_meta_data(input_filename):
@@ -57,21 +69,43 @@ def assure_that_filename_is_pdf(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        file_ = request.files['file_1']
-        if file_ and allowed_file(file_.filename):
-            if request.form.get('filename_1'):
-                filename = secure_filename(request.form['filename_1'])
-                filename = assure_that_filename_is_pdf(filename)
+        uploaded_files = request.files.getlist('files')
+        if not validate_files(uploaded_files):
+            flash(u"Spróbuj jeszcze raz", "info")
+            return redirect(url_for('upload_file'))
+        if not uploaded_files:
+            flash(u"Nie wskazano żadnych plików", "warning")
+            return redirect(url_for("upload_file"))
+        if len(uploaded_files) == 1:
+            if request.form.get('filename'):
+                filename = secure_filename(request.form['filename'])
             else:
-                filename = secure_filename(file_.filename)
-            file_.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                filename = secure_filename(uploaded_files[0].filename)
+            uploaded_files[0].save(os.path.join(app.config['UPLOAD_FOLDER'],
+                                                filename))
             clean_meta_data(filename)
             return redirect(url_for('download_file_from_uploads',
                                     filename=filename))
         else:
-            flash('Niepoprawny plik', 'danger')
-            return redirect(url_for('upload_file'))
-    return render_template('upload.html')
+            for uploaded_file in uploaded_files:
+                uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'],
+                                                uploaded_file.filename))
+                clean_meta_data(uploaded_file.filename)
+            if request.form.get('filename'):
+                arch_name = request.form['filename'] + '.zip'
+            else:
+                arch_name = "pdf_files.zip"
+            zip_arch = zipfile.ZipFile(
+                os.path.join(app.config['UPLOAD_FOLDER'], arch_name), "w"
+            )
+            for uploaded_file in uploaded_files:
+                zip_arch.write(os.path.join(app.config['UPLOAD_FOLDER'],
+                                            uploaded_file.filename),
+                               arcname=uploaded_file.filename)
+            zip_arch.close()
+            return redirect(url_for('download_file_from_uploads',
+                                    filename=arch_name))
+    return render_template("upload.html")
 
 
 @app.route('/uploads/<path:filename>')
